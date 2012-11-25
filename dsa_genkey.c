5fe3103393a6a1363e526553e456fa314274198f
Hannes Gustafsson <hnnsgstfssn@gmail.com>
2012-11-25 19:19:13 +0100

    Use /dev/random to seed key generation

--- dsa_genkey.c
+++ dsa_genkey.c
@@ -1,10 +1,16 @@
 #include "dsa.h"
+#include <fcntl.h>
+#include <unistd.h>
 
 void dsa_genkey(DSA *dsa){
   assert(dsa != NULL);
   gmp_randstate_t rs;
   gmp_randinit_default(rs);
-  gmp_randseed_ui(rs, arc4random());
+  int source = open("/dev/random", O_RDONLY);
+  int seed;
+  read(source, &seed, sizeof(seed));
+  close(source);
+  gmp_randseed_ui(rs, seed);
 
   mpz_set(dsa->x, dsa->q);
   mpz_sub_ui(dsa->x, dsa->q, 1);
